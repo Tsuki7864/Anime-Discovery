13583a3c441b0c6f8fe101fe100ff8a6833e0b11
Ali <27aindia@cpsd.us>
2026-04-07 13:07:48 -0400

    minor fixes

--- js/script.js
+++ js/script.js
@@ -308,7 +308,7 @@ async function loadSearchPage(query, pageNumber) {
     if (!grid) return;
 
     currentQuery = query;
-    currentPage = pageNumber;
+    currentPage = parseInt(pageNumber, 10);
 
     // --- ADD THESE TWO LINES FOR THE BACK BUTTON ---
     const newUrl = `${window.location.pathname}?q=${encodeURIComponent(query)}&page=${pageNumber}`;
@@ -319,7 +319,7 @@ async function loadSearchPage(query, pageNumber) {
     // Fetch using your teammate's bulletproof function + dynamic Safe Search
 
     let results = await fetchFromJikan(
-        `/anime?q=${encodeURIComponent(currentQuery)}&limit=15&page=${currentPage}${getSfwString()}`
+        `/anime?q=${encodeURIComponent(currentQuery)}&limit=18&page=${currentPage}${getSfwString()}`
     );
     results = filterExplicitContent(results);
     if (results.length === 0) {
